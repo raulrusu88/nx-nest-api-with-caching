6c8f0dad372f7d069793fac91dbdc4b87c78fc4a
hazelnutx <raulrusu46@gmail.com>
2021-11-02 11:26:37 +0200

    feat: implemented core lib

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from '@nx-nest-api-with-caching/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
